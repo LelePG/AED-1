93605679e63c87bbbf5dcb1193ed5afa5b602393
Leticia <Tichwhovian@gmail.com>
2019-04-13 13:18:22 -0300

    Mostrar detalhes de uma pessoa

--- Leticia_agenda.c
+++ Leticia_agenda.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define p_pos (pessoa*)procura+20;//pra nao precisar ficar voltando
 
 typedef struct{
 char nome[20];
@@ -8,53 +9,105 @@ int telefone;
 int idade;
 } pessoa;
 
-void menu(void);
-void imprime_pessoa(void);
+void menu(int *op);
+void imprime_pessoas(void);
 void remove_pessoa(void);
 void adiciona_pessoa(void);
+void mostra_pessoa(void);
+void arruma_ponteiros(void* pBuffer);
+void verifica(void* pBuffer);//alocaçao
+void p_final(void);//coloca p no fim do buffer
 
-int *cont,*cont2,*cont_p,*op;
 void *pBuffer;
+int *cont,*cont2,*cont_p,*op;
+char *procura;//20 pos como o nome
 pessoa *p;
 
 int main(){
-   pBuffer = (void*) malloc (4*sizeof(int));//aloco mem�ria para as minhas 4 vari�veis
-   cont = pBuffer;
-   cont2 = cont+1;
-   cont_p = cont2+1;
-   op = cont_p +1;
-   p =op+1;//todas as variaveis colocadas no lugar
-   *cont_p = 0;//coloca o valor na quantidade de pessoas
+    pBuffer = (void*) malloc (4*sizeof(int) + 20*sizeof(char));
+    verifica(pBuffer);//aloco memória para as minhas 4 variáveis
+    arruma_ponteiros(pBuffer);
+    *cont_p = 0;
     while(1){
-        menu();
-        scanf("%d",op);
-        getchar();//tira o \n que buga a entrada do nome
-        if(*op ==1){
-            adiciona_pessoa();
+        menu(op);
+        switch(*op){
+        	case 1:
+        		adiciona_pessoa();
+        		break;
+        	case 2:
+        		remove_pessoa();
+        		break;
+        	case 3:
+        		imprime_pessoas();
+        		break;
+            case 4:
+                mostra_pessoa();
+                break;
+        	case 5:
+        		free(pBuffer);
+        		return 0;
+        	default:
+        		printf("Opção inválida\n");
+
         }
-        else if(*op==2){
-            remove_pessoa();
-        }
-        else if(*op==3){
-            imprime_pessoa();
-        }
-        else{
-            return 0;
-        }
+
    }
 
 }
 
-void menu (void){
+
+
+void menu (int *op){
 printf("\n-----AGENDA-----\n");
 printf("1- Adicionar pessoa\n");
 printf("2- Remover pessoa\n");
 printf("3- Imprimir lista\n");
-printf("4- Sair\n");
+printf("4- Imprimir pessoa\n");
+printf("5- Sair\n");
+scanf("%d",op);
+getchar();// tira o barra n
 }
 
-void imprime_pessoa(void){//Tá funcionando direitinho
-    p = op+1;//coloca p na posição original dele pra ir avançando pelo buffer
+void verifica(void* buffer){
+if(buffer==NULL){
+		printf("ERRO! Saindo do programa.\n");
+		exit(1);
+	}
+}
+
+void arruma_ponteiros(void* pBuffer){
+    cont = pBuffer;
+    cont2 = cont+1;
+    cont_p = cont2+1;
+    op = cont_p +1;
+    procura =(char*) op+1;
+    p =(pessoa*)procura+20;
+}
+
+
+void adiciona_pessoa(void){//tenho que lidar com a mudança de endereço, mas desisto 4 now
+    *cont_p=*cont_p+1;//atualizo o contador de pessoas
+    pBuffer  = (void*) realloc (pBuffer,4*sizeof(int) + 20*sizeof(char)+(*cont_p)*sizeof(pessoa));//atualizo o tamanho do buffer
+    verifica(pBuffer);
+    arruma_ponteiros(pBuffer);
+    p_final();
+    printf("Nome: ");//colect data
+    scanf("%[^\n]",p->nome);
+    printf("Idade: ");
+    scanf("%d",&p->idade);
+    printf("Telefone: ");
+    scanf("%d",&p->telefone);
+    printf("---NOVO INDIVÍDUO ADICIONADO---\n");
+}
+
+void p_final(void){//leva p pro final
+    p = p_pos;
+    for(*cont = 1;*cont<*cont_p;*cont = *cont+1){
+        p++;
+    }
+}
+void imprime_pessoas(void){//Tá funcionando direitinho
+    p = p_pos;//coloca p na posição original dele pra ir avançando pelo buffer
     for(*cont =0;*cont<*cont_p;*cont=(*cont)+1){
         printf("--PESSOA %d--\n",*cont+1);//Pessoa 1,2,3...
         printf("Nome: %s\n",p->nome);
@@ -64,35 +117,40 @@ void imprime_pessoa(void){//Tá funcionando direitinho
     }
 }
 
-
-void remove_pessoa(void){//a princípio tá funcionando, mas o cont_p tá ficando meio louco.
-char compara[20];//ok funciona, como eu tiro isso daqui ahora;
-printf("Digite quem deseja excluir: ");
-scanf("%[^\n]",compara);
- p = op+1;//coloco o ponteiro de pessoas no lugar dele
- for(*cont =0;*cont<*cont_p;*cont=(*cont)+1){//procura pelo buffer
-       if(strcmp(p->nome,compara)==0){//se achou
+void remove_pessoa(void){//a princípio tá funcionando
+    printf("Digite quem deseja excluir: ");
+    scanf("%[^\n]",procura);
+    p =p_pos;//coloco o ponteiro de pessoas no lugar dele
+    for(*cont =0;*cont<*cont_p;*cont=(*cont)+1){//procura pelo buffer
+       if(strcmp(p->nome,procura)==0){//se achou
         for(*cont2 = *cont;*cont2<*cont_p;*cont2++){
-            p = p+*cont2;//puxa todas uma casa pra trás
+            *p = *(p+1);//puxa todas uma casa pra trás
+            p++;//e avança o ponteiro
         }
         *cont_p = *cont_p -1;//arruma o contador de pessoas
-        pBuffer  = (void*) realloc (pBuffer,4*sizeof(int)+(*cont_p)*sizeof(pessoa));//realoca o buffer, excluindo a última posição que vai ter lixo
-        printf("-- %s EXCLUÍDO --\n",compara);
+        pBuffer  = (void*) realloc (pBuffer,4*sizeof(int) + 20*sizeof(char)+(*cont_p)*sizeof(pessoa));//atualizo o tamanho do buffer
+        verifica(pBuffer);
+        printf("-- %s EXCLUÍDO --\n",procura);
         return;
        }
+    p++;
     }
+    printf("Name not found");
 }
 
-
-void adiciona_pessoa(void){//terceira pessoa adicionada o valor fica estranho, mas não muda nenhum endereço....
-    *cont_p=*cont_p+1;//atualizo o contador de pessoas
-    pBuffer  = (void*) realloc (pBuffer,4*sizeof(int)+(*cont_p)*sizeof(pessoa));//atualizo o tamanho do buffer
-    printf("Nome: ");//colect data
-    scanf("%[^\n]",p->nome);
-    printf("Idade: ");
-    scanf("%d",&p->idade);
-    printf("Telefone: ");
-    scanf("%d",&p->telefone);
-    p++;//p is buffer's last position
-    printf("---NOVO INDIVÍDUO ADICIONADO---\n");
+void mostra_pessoa(void){
+    printf("Digite o nome da pessoa que vc quer ver os dados: ");
+    scanf("%[^\n]",procura);
+    p =p_pos;//coloco o ponteiro de pessoas no lugar dele
+    for(*cont =0;*cont<*cont_p;*cont=(*cont)+1){//procura pelo buffer
+        if(strcmp(p->nome,procura)==0){//se achou
+            printf("----Pessoa encontrada-----\n");
+            printf("Nome : %s\n",p->nome);
+            printf("Idade : %d\n",p->idade);
+            printf("Telefone: %d\n",p->telefone);
+            return;
+        }
+    p++;
+    }
+    printf("----Pessoa nao encontrada-----\n");
 }
