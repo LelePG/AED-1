37b96af6d56f74a81d20cd39b003d5e27fe68cab
Leticia <Tichwhovian@gmail.com>
2019-04-25 21:49:19 -0300

    Insertion Sort por idade

--- Leticia_agenda.c
+++ Leticia_agenda.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define p_pos (pessoa*)procura+20;//pra nao precisar ficar voltando
+#define p_pos (pessoa*)procura+1;//Definindo a posicao de p porque uso isso varias vezes no codigo
 
 typedef struct{
 char nome[20];
@@ -14,18 +14,22 @@ void imprime_pessoas(void);
 void remove_pessoa(void);
 void adiciona_pessoa(void);
 void mostra_pessoa(void);
-void arruma_ponteiros(void* pBuffer);
-void verifica(void* pBuffer);//alocaçao
+void arruma_ponteiros(void* pBuffer);//coloca os ponteiros no lugar
+void verifica(void* pBuffer);//verifica alocaçao
 void p_final(void);//coloca p no fim do buffer
+void ordena_insertion(void);
+void ordena_bubble(void);
+void ordena_selection(void);
+
 
 void *pBuffer;
 int *cont,*cont2,*cont_p,*op;
-char *procura;//20 pos como o nome
+pessoa *procura;//variavel auxiliar
 pessoa *p;
 
 int main(){
-    pBuffer = (void*) malloc (4*sizeof(int) + 20*sizeof(char));
-    verifica(pBuffer);//aloco memória para as minhas 4 variáveis
+    pBuffer = (void*) malloc (4*sizeof(int) + sizeof(pessoa));//a variavel pessoa alocada aqui eh a minha auxiliar
+    verifica(pBuffer);
     arruma_ponteiros(pBuffer);
     *cont_p = 0;
     while(1){
@@ -43,7 +47,16 @@ int main(){
             case 4:
                 mostra_pessoa();
                 break;
-        	case 5:
+            case 5:
+        		ordena_insertion();
+        		break;
+            case 6:
+                ordena_bubble();
+                break;
+        	case 7:
+        		ordena_selection();
+        		break;
+        	case 8:
         		free(pBuffer);
         		return 0;
         	default:
@@ -57,40 +70,39 @@ int main(){
 
 
 
-void menu (int *op){
+void menu (int *op){//OK
 printf("\n-----AGENDA-----\n");
 printf("1- Adicionar pessoa\n");
 printf("2- Remover pessoa\n");
 printf("3- Imprimir lista\n");
 printf("4- Imprimir pessoa\n");
-printf("5- Sair\n");
+printf("5- Ordena com Insertion Sort\n");
+printf("6- Ordena com Bubble Sort\n");
+printf("7- Ordena com Selection Sort\n");
+printf("8- Sair\n");
 scanf("%d",op);
 getchar();// tira o barra n
 }
-
-void verifica(void* buffer){
+void verifica(void* buffer){//OK
 if(buffer==NULL){
 		printf("ERRO! Saindo do programa.\n");
 		exit(1);
 	}
 }
-
-void arruma_ponteiros(void* pBuffer){
+void arruma_ponteiros(void* pBuffer){//OK
     cont = pBuffer;
     cont2 = cont+1;
     cont_p = cont2+1;
     op = cont_p +1;
-    procura =(char*) op+1;
-    p =(pessoa*)procura+20;
+    procura =(pessoa*) op+1;//vou usar como variavel pessoa auxiliar
+    p =(pessoa*)procura+1;
 }
-
-
-void adiciona_pessoa(void){//tenho que lidar com a mudança de endereço, mas desisto 4 now
+void adiciona_pessoa(void){//OK
     *cont_p=*cont_p+1;//atualizo o contador de pessoas
-    pBuffer  = (void*) realloc (pBuffer,4*sizeof(int) + 20*sizeof(char)+(*cont_p)*sizeof(pessoa));//atualizo o tamanho do buffer
+    pBuffer  = (void*) realloc (pBuffer,4*sizeof(int) +(*cont_p+1)*sizeof(pessoa));//atualizo o tamanho do buffer
     verifica(pBuffer);
     arruma_ponteiros(pBuffer);
-    p_final();
+    p_final();//p tem que ir pro final pra mim adicionar a nova pessoa na ultima posicao
     printf("Nome: ");//colect data
     scanf("%[^\n]",p->nome);
     printf("Idade: ");
@@ -99,14 +111,13 @@ void adiciona_pessoa(void){//tenho que lidar com a mudança de endereço, mas de
     scanf("%d",&p->telefone);
     printf("---NOVO INDIVÍDUO ADICIONADO---\n");
 }
-
-void p_final(void){//leva p pro final
+void p_final(void){//OK
     p = p_pos;
     for(*cont = 1;*cont<*cont_p;*cont = *cont+1){
         p++;
     }
 }
-void imprime_pessoas(void){//Tá funcionando direitinho
+void imprime_pessoas(void){//OK
     p = p_pos;//coloca p na posição original dele pra ir avançando pelo buffer
     for(*cont =0;*cont<*cont_p;*cont=(*cont)+1){
         printf("--PESSOA %d--\n",*cont+1);//Pessoa 1,2,3...
@@ -116,34 +127,34 @@ void imprime_pessoas(void){//Tá funcionando direitinho
         p++;//p vai assumindo os valores que eu preciso apresentar, no final ele para no fim do buffer
     }
 }
-
-void remove_pessoa(void){//a princípio tá funcionando
+void remove_pessoa(void){//PROBLEMA tava tudo certo antes, agora ta dando problema.
     printf("Digite quem deseja excluir: ");
-    scanf("%[^\n]",procura);
+    scanf("%[^\n]",procura->nome);
     p =p_pos;//coloco o ponteiro de pessoas no lugar dele
     for(*cont =0;*cont<*cont_p;*cont=(*cont)+1){//procura pelo buffer
-       if(strcmp(p->nome,procura)==0){//se achou
+       if(strcmp(p->nome,procura->nome)==0){//se achou
         for(*cont2 = *cont;*cont2<*cont_p;*cont2++){
             *p = *(p+1);//puxa todas uma casa pra trás
             p++;//e avança o ponteiro
         }
-        *cont_p = *cont_p -1;//arruma o contador de pessoas
-        pBuffer  = (void*) realloc (pBuffer,4*sizeof(int) + 20*sizeof(char)+(*cont_p)*sizeof(pessoa));//atualizo o tamanho do buffer
+
+        pBuffer  = (void*) realloc (pBuffer,4*sizeof(int) + (*cont_p)*sizeof(pessoa));//atualizo o tamanho do buffer
         verifica(pBuffer);
-        printf("-- %s EXCLUÍDO --\n",procura);
+        arruma_ponteiros(pBuffer);
+        *cont_p = *cont_p -1;//arruma o contador de pessoas
+        printf("-- %s EXCLUÍDO --\n",procura->nome);
         return;
        }
     p++;
     }
     printf("Name not found");
 }
-
-void mostra_pessoa(void){
+void mostra_pessoa(void){//OK
     printf("Digite o nome da pessoa que vc quer ver os dados: ");
-    scanf("%[^\n]",procura);
+    scanf("%[^\n]",procura->nome);
     p =p_pos;//coloco o ponteiro de pessoas no lugar dele
     for(*cont =0;*cont<*cont_p;*cont=(*cont)+1){//procura pelo buffer
-        if(strcmp(p->nome,procura)==0){//se achou
+        if(strcmp(p->nome,procura->nome)==0){//se achou
             printf("----Pessoa encontrada-----\n");
             printf("Nome : %s\n",p->nome);
             printf("Idade : %d\n",p->idade);
@@ -154,3 +165,24 @@ void mostra_pessoa(void){
     }
     printf("----Pessoa nao encontrada-----\n");
 }
+void ordena_insertion(void){//OK, ordena pela idade
+p = p_pos;
+for(*cont =0;*cont<*cont_p-1;(*cont)++){
+    *procura = *(p + (*cont+1));
+    *cont2 = *cont ;
+      while ((*cont2) >= 0 && procura->idade < (p+(*cont2))->idade) {
+        *(p +*cont2+1) = *(p+*cont2);
+        (*cont2)--;
+      }
+
+      *(p+1+*cont2) = *procura;
+   }
+printf("-----Lista ordenada----\n");
+}
+void ordena_bubble(void){//FAZER
+printf("bubble sort");
+}
+void ordena_selection(void){//FAZER
+printf("selection sort");
+}
+
